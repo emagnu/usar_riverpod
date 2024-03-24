6bde8e2fad555077015d03f8c067879d618f2d86
Emmanuele <emagnubcp@gmail.com>
2024-03-24 18:15:46 +0000

    AppRouter como Provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 //  Import FILES
-import 'config/router/app_router.dart';
-import 'config/theme/app_theme.dart';
+// import 'config/router/app_router.dart';
+// import 'config/theme/app_theme.dart';
+import './config/config.dart';
 //  PARTS
 //  PROVIDERS
 //  //  ///
 
 void main() => runApp(const ProviderScope(child: MyApp()));
 
-class MyApp extends StatelessWidget {
+// class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appRouter = ref.watch(appRouterProvider);
+
     return MaterialApp.router(
       title: 'Riverpod Providers',
       debugShowCheckedModeBanner: false,
+      // routerConfig: appRouter,
       routerConfig: appRouter,
       theme: AppTheme(isDarkmode: false).getTheme(),
     );
--- lib/presentation/screens/02_provider_router/router_screen.dart
+++ lib/presentation/screens/02_provider_router/router_screen.dart
@@ -1,24 +1,30 @@
 //  //   ///
 //  Import LIBRARIES
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:go_router/go_router.dart';
 //  Import FILES
+import '../../../config/router/app_router.dart';
 //  PARTS
 //  PROVIDERS
 //  //  ///
 
-class RouterScreen extends StatelessWidget {
+// class RouterScreen extends StatelessWidget {
+class RouterScreen extends ConsumerWidget {
   const RouterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Provider + Go Router'),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_back_ios_new_rounded),
-        onPressed: () {},
+        onPressed: () {
+          // context.pop();
+          ref.read(appRouterProvider).pop();
+        },
       ),
     );
   }
